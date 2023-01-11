b07d58dc21e9927eaa81179a589abb402ffee221
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-11 15:47:27 +0000

    integrate crispy-forms into the booking app

--- f1_ticket_booking_system/settings.py
+++ f1_ticket_booking_system/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'race_details',
     'booking',
 ]
@@ -56,6 +57,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
