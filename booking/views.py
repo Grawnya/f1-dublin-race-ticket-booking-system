337e1d9179cdf7097704c051d288a1461503b734
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-14 21:45:13 +0000

    create post function for CreateProfile class in booking views

--- booking/views.py
+++ booking/views.py
@@ -17,3 +17,21 @@ class CreateProfile(View):
                       {
                         'profile_form': WebsiteUserForm()
                       })
+
+    def post(self, request):
+        profile_form = WebsiteUserForm(data=request.POST)
+
+        if profile_form.is_valid():
+            profile_first_name = request.POST.get('first_name')
+            profile_last_name = request.POST.get('last_name')
+            profile_email = request.POST.get('email')
+            profile_fave_team = request.POST.get('fave_team')
+            profile_nationality = request.POST.get('nationality')
+            profile_form.save()
+        else:
+            profile_form = WebsiteUserForm()
+        return render(request,
+                      'profile.html',
+                      {
+                        'profile_form': WebsiteUserForm()
+                      })
