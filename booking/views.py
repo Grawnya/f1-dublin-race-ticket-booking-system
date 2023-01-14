80807e890d84bc59ae596cd39442f771e3a8588f
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-14 21:37:09 +0000

    create get function for CreateProfile class in booking views

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render, redirect
 from django.views import generic, View
+from .models import WebsiteUser
+from .forms import WebsiteUserForm
 
 # Create your views here.
+class CreateProfile(View):
+    
+    def get(self, request):
+        queryset = WebsiteUser.objects.filter(email=request.user.email).exists()
+        if queryset:
+            profile_form = WebsiteUserForm(instance=WebsiteUser.objects.filter(email=request.user.email).first())
+        else:
+            profile_form = WebsiteUserForm()
+        return render(request,
+                      'profile.html',
+                      {
+                        'profile_form': WebsiteUserForm()
+                      })
