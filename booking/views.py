10cbd147cbdcfd2fe3d00a2903e6ed21a526495e
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-15 10:25:17 +0000

    update username data entry value in website user model from profile form

--- booking/views.py
+++ booking/views.py
@@ -8,10 +8,10 @@ class CreateProfile(View):
     
     def get(self, request):
         queryset = WebsiteUser.objects.filter(username=request.user.username).exists()
+        print(queryset)
         if queryset:
             website_user = WebsiteUser.objects.filter(username=request.user.username).first()
             profile_form = WebsiteUserForm(instance=website_user)
-            pass
         else:
             profile_form = WebsiteUserForm()
         return render(request,
@@ -24,7 +24,7 @@ class CreateProfile(View):
         profile_form = WebsiteUserForm(data=request.POST)
 
         if profile_form.is_valid():
-            profile_username = request.user.username
+            profile_form.instance.username = request.user.username
             profile_first_name = request.POST.get('first_name')
             profile_last_name = request.POST.get('last_name')
             profile_email = request.POST.get('email')
