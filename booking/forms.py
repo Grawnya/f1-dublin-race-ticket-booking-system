f29907b3380c243212cda25f6ed65b3fe1aac051
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-18 16:28:47 +0000

    update labels of ticket form for booking

--- booking/forms.py
+++ booking/forms.py
@@ -16,6 +16,12 @@ class WebsiteUserForm(forms.ModelForm):
 class TicketForm(forms.ModelForm):
    class Meta:
         model = Ticket
+        labels = {
+         'for_self': 'Are you booking for yourself?',
+         'first_name': 'First Name',
+         'last_name': 'Last Name',
+         'fave_team': 'Favourite Team',
+         'show': 'Do you want to share your ticket on the Tickets Sold page to let people know you are going?'
+        }
         fields = ('for_self','first_name', 'last_name', 'nickname', 'fave_team',
-                  'nationality')
-
+                  'nationality', 'show')
