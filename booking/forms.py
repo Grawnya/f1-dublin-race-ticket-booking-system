b17990b26ce135f9d64cd0c95ffaa7b80ddd61bc
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-18 14:38:57 +0000

    create new crispy TicketForm in forms.py

--- booking/forms.py
+++ booking/forms.py
@@ -1,8 +1,14 @@
-from .models import WebsiteUser
+from .models import WebsiteUser, Ticket
 from django import forms
 
 class WebsiteUserForm(forms.ModelForm):
    class Meta:
         model = WebsiteUser
         fields = ('first_name', 'last_name', 'email', 'fave_team',
+                  'nationality')
+
+class TicketForm(forms.ModelForm):
+   class Meta:
+        model = Ticket
+        fields = ('first_name', 'last_name', 'nickname', 'fave_team',
                   'nationality')
