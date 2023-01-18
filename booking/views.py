4249ab9e7e1d73d06ec8be3dfee30c7243ea6036
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-18 22:22:24 +0000

    update SeeMyTickets class to view my tickets

--- booking/views.py
+++ booking/views.py
@@ -31,7 +31,8 @@ class CreateProfile(View):
                             'signup.html',)
 
     def post(self, request):
-        profile_form = WebsiteUserForm(data=request.POST)
+        website_user = WebsiteUser.objects.filter(username=request.user.username).first()
+        profile_form = WebsiteUserForm(data=request.POST, instance=website_user)
 
         if profile_form.is_valid():
             profile_form.instance.username = request.user.username
@@ -78,12 +79,12 @@ class EditProfile(View):
                       })
 
 
-class SeeTickets(generic.ListView):
+class SeeMyTickets(generic.ListView):
     model = Ticket
     template_name = 'my_tickets.html'
 
-    # def get_queryset(self):
-    #    return super(SeeTickets, self).get_queryset().filter(booked_by=self.request.user.username)
+    def get_queryset(self):
+       return super(SeeMyTickets, self).get_queryset().filter(booked_by=self.request.user.username).order_by('booked_on')
 
 class NewTicket(View):
 
@@ -105,39 +106,6 @@ class NewTicket(View):
         else:
             url = reverse('index.html')
             return HttpResponseRedirect(url)
-
-    def post(self, request):
-        ticket_form = Ticket(data=request.POST)
-
-        if ticket_form.is_valid():
-            ticket_form_for_self = request.POST.get('for_self')
-            ticket_form.instance.booked_by = request.user.username
-            ticket_form_first_name = request.POST.get('first_name')
-            ticket_form_last_name = request.POST.get('last_name')
-            ticket_form_nickname = request.POST.get('nickname')
-            ticket_form_fave_team = request.POST.get('fave_team')
-            ticket_form_nationality = request.POST.get('nationality')
-            ticket_form_show = request.POST.get('show')
-        else:
-            ticket_form = TicketForm()
-        ticket_form.save()
-        return render(request,
-                      'my_tickets.html',
-                      {
-                        'ticket_form': ticket_form
-                      })
-
-class EditTicket(View):
-
-    def get(self, request, item_id):
-        item = get_object_or_404(Ticket, id=item_id)
-        ticket_form = TicketForm(instance=item)
-        return render(request,
-                            'edit_ticket.html',
-                            {
-                                'ticket_form': ticket_form
-                            })
-    
 
     def post(self, request):
         ticket_form = Ticket(data=request.POST)
