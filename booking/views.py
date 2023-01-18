f0457ec9ed0ed88a91d94f90aeff6e9547eee9a2
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-18 19:18:15 +0000

    remove edit profile from navbar and read in user values into profiel if they exist

--- booking/views.py
+++ booking/views.py
@@ -2,18 +2,33 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic, View
 from .models import WebsiteUser, Ticket
 from .forms import WebsiteUserForm, TicketForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
 class CreateProfile(View):
     
     def get(self, request):
         if request.user.is_authenticated:
-            profile_form = WebsiteUserForm()
+            website_users = WebsiteUser.objects.filter(username=request.user.username).exists()
+            if website_users:
+                user_exists = get_object_or_404(WebsiteUser, username=request.user.username)
+                profile_form = WebsiteUserForm(instance=user_exists)
+                return render(request,
+                            'profile.html',
+                            {
+                                'profile_form': profile_form
+                            })
+            else:
+                profile_form = WebsiteUserForm()
+                return render(request,
+                            'edit_profile.html',
+                            {
+                                'profile_form': profile_form
+                            })
+        else:
             return render(request,
-                        'profile.html',
-                        {
-                            'profile_form': profile_form
-                        })
+                            'signup.html',)
 
     def post(self, request):
         profile_form = WebsiteUserForm(data=request.POST)
@@ -47,6 +62,7 @@ class EditProfile(View):
                             'profile_form': profile_form
                         })
 
+
     def post(self, request):
         profile_form = get_object_or_404(WebsiteUser, username=request.user.username)
         profile_first_name = request.POST.get('first_name')
@@ -73,12 +89,21 @@ class NewTicket(View):
 
     def get(self, request):
         if request.user.is_authenticated:
-            ticket_form = TicketForm()
-            return render(request,
-                        'new_ticket.html',
-                        {
-                            'ticket_form': ticket_form
-                        })
+            website_user = WebsiteUser.objects.filter(username=request.user.username).exists()
+            if website_user:
+                item = get_object_or_404(WebsiteUser, username=request.user.username)
+                
+                ticket_form = TicketForm(instance=item)
+                return render(request,
+                            'new_ticket.html',
+                            {
+                                'ticket_form': ticket_form
+                            })
+            else:
+                pass
+        else:
+            url = reverse('index.html')
+            return HttpResponseRedirect(url)
 
     def post():
         pass
