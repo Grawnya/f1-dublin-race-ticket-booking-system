9b91d77cba4937f78b6542e879cd6ffadcd92a6c
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-20 14:43:29 +0000

    create seat_choices and stand_choices in models.py

--- booking/models.py
+++ booking/models.py
@@ -278,6 +278,116 @@ for_self_choices = (
     (False, 'No')
 )
 
+stand_choices = (
+    ("A", "A"),
+    ("B", "B"),
+    ("C", "C"),
+    ("D", "D"),
+)
+
+seat_choices = (
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+    (6, 6),
+    (7, 7),
+    (8, 8),
+    (9, 9),
+    (10, 10),
+    (11, 11),
+    (12, 12),
+    (13, 13),
+    (14, 14),
+    (15, 15),
+    (16, 16),
+    (17, 17),
+    (18, 18),
+    (19, 19),
+    (20, 20),
+    (21, 21),
+    (22, 22),
+    (23, 23),
+    (24, 24),
+    (25, 25),
+    (26, 26),
+    (27, 27),
+    (28, 28),
+    (29, 29),
+    (30, 30),
+    (31, 31),
+    (32, 32),
+    (33, 33),
+    (34, 34),
+    (35, 35),
+    (36, 36),
+    (37, 37),
+    (38, 38),
+    (39, 39),
+    (40, 40),
+    (41, 41),
+    (42, 42),
+    (43, 43),
+    (44, 44),
+    (45, 45),
+    (46, 46),
+    (47, 47),
+    (48, 48),
+    (49, 49),
+    (50, 50),
+    (51, 51),
+    (52, 52),
+    (53, 53),
+    (54, 54),
+    (55, 55),
+    (56, 56),
+    (57, 57),
+    (58, 58),
+    (59, 59),
+    (60, 60),
+    (61, 61),
+    (62, 62),
+    (63, 63),
+    (64, 64),
+    (65, 65),
+    (66, 66),
+    (67, 67),
+    (68, 68),
+    (69, 69),
+    (70, 70),
+    (71, 71),
+    (72, 72),
+    (73, 73),
+    (74, 74),
+    (75, 75),
+    (76, 76),
+    (77, 77),
+    (78, 78),
+    (79, 79),
+    (80, 80),
+    (81, 81),
+    (82, 82),
+    (83, 83),
+    (84, 84),
+    (85, 85),
+    (86, 86),
+    (87, 87),
+    (88, 88),
+    (89, 89),
+    (90, 90),
+    (91, 91),
+    (92, 92),
+    (93, 93),
+    (94, 94),
+    (95, 95),
+    (96, 96),
+    (97, 97),
+    (98, 98),
+    (99, 99),
+    (100, 100)
+)
+
 # Create your models here.
 class WebsiteUser(models.Model):
     '''WebsiteUser Information Model'''
@@ -315,8 +425,8 @@ class Ticket(models.Model):
     nickname = models.CharField(max_length=250)
     fave_team = models.CharField(max_length=250, choices=team_choices, default='alfa_romeo')
     nationality = models.CharField(max_length=250, choices=country_choices, default='irl')
-    seat_number = models.IntegerField(null=True)
-    stand = models.CharField(max_length=1, null=True)
+    seat_number = models.IntegerField(choices=seat_choices)
+    stand = models.CharField(max_length=1, choices=stand_choices)
     show = models.BooleanField(default=True, choices=for_self_choices)
 
     def __str__(self):
