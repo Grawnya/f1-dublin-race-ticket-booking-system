1a6cb8e1e7fd4912b9e52bfeb343fbd5b017ffd4
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-20 20:34:39 +0000

    remove the show seat rect in svg and show seating image next to it instead

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,6 @@
-function clickHandler(element) {
-    stand_name = element.target.id;
-    var letters = ["A", "B", "C", "D"];
-    for (let i = 0; i < letters.length; i++) {
-        if (stand_name === letters[i]) {
-
-        }
-    }
+function seeSeatChart(element, track) {
+    var seat_button = element.target.id;
 }
+
+const trackLayout = document.getElementById("track_image").innerHTML;
+document.addEventListener("click", seeSeatChart)
