2a3e705387b5ac70be735684bef98502f51f44d4
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-22 14:07:42 +0000

    update function and class names to match show_attendees app

--- show_attendees/test_views.py
+++ show_attendees/test_views.py
@@ -2,12 +2,12 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 # Create your tests here.
-class TestRaceDetailsViews(TestCase):
+class TestShowAttendeesViews(TestCase):
     def setUp(self):
         self.client = Client()
         self.show_tickets_url = reverse('show_attendees')
 
-    def test_homepage_GET(self):
+    def test_show_attendees_GET(self):
         response = self.client.get(self.show_tickets_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'show_attendees.html')
