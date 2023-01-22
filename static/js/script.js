64d69b81e549d37b0bb820fefbb6ea174902359c
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-22 23:47:01 +0000

    update function to change track image

--- static/js/script.js
+++ static/js/script.js
@@ -8,12 +8,13 @@ setTimeout(function() {
 // function to change track image on main page
 function changeTrackImage(element) {
     var track_id = element.target.id
-    console.log(track_id)
     if (track_id === 'track_graphic') {
-        document.getElementById('track_graphic').innerHTML = '<img src="https://res.cloudinary.com/grawnya/image/upload/v1674393438/formula1_dublin_street_race_map_vpf8oo.png" alt="Track mapped onto Google Maps">'
+        document.getElementById('track_graphic').setAttribute("src", "https://res.cloudinary.com/grawnya/image/upload/v1674393438/formula1_dublin_street_race_map_vpf8oo.png")
+        document.getElementById('track_graphic').setAttribute("alt", "Track mapped onto Google Maps")
         document.getElementById('track_graphic').setAttribute('id', 'track_map')}
-    else if (track_id === 'track_map') {
-        document.getElementById('track_map').innerHTML = '<img src="https://res.cloudinary.com/grawnya/image/upload/v1674393431/track_with_stands_fok6ra.png" alt="Track image with stands">'
+        else if (track_id === 'track_map') {
+        document.getElementById('track_map').setAttribute("src", "https://res.cloudinary.com/grawnya/image/upload/v1674393431/track_with_stands_fok6ra.png")
+        document.getElementById('track_map').setAttribute("alt", "Track image with stands")
         document.getElementById('track_map').setAttribute('id', 'track_graphic')
     }
 }
