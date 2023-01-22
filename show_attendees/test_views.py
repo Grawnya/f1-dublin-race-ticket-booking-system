bc4fc6fc53c21eb5a9a16544c24ab6917a83cd62
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-22 14:03:18 +0000

    test views.py in show_attendees app

---
+++ show_attendees/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+# Create your tests here.
+class TestRaceDetailsViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.show_tickets_url = reverse('show_attendees')
+
+    def test_homepage_GET(self):
+        response = self.client.get(self.show_tickets_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'show_attendees.html')
