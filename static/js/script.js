3226d7f5c9f7d65abf55d23793a9cca0739512b8
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-22 23:14:20 +0000

    add js functinality to the website to change track image

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,20 @@
-function seeSeatChart(element, track) {
-    var seat_button = element.target.id;
+// Function to automatically dismiss the alert after 3 seconds
+setTimeout(function() {
+    var messages = document.getElementById("msg");
+    var alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 3000);
+
+// function to change track image on main page
+function changeTrackImage(e) {
+    var track_id = e.target.id
+    if (track_id === 'track_graphic') {
+        document.getElementById('track_graphic').innerHTML = '<img src="https://res.cloudinary.com/grawnya/image/upload/v1674393438/formula1_dublin_street_race_map_vpf8oo.png" alt="Track mapped onto Google Maps">'
+        document.getElementById('track_graphic').setAttribute('id', 'track_map')}
+    else if (track_id === 'track_map') {
+        document.getElementById('track_map').innerHTML = '<img src="https://res.cloudinary.com/grawnya/image/upload/v1674393431/track_with_stands_fok6ra.png" alt="Track image with stands">'
+        document.getElementById('track_map').setAttribute('id', 'track_graphic')
+    }
 }
 
-const trackLayout = document.getElementById("track_image").innerHTML;
-document.addEventListener("click", seeSeatChart)
+document.addEventListener('click', changeTrackImage)
