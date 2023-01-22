5a0209e25d234129be21e75c9a702423d00e8cd8
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-22 13:59:10 +0000

    test views.py in race_details app

---
+++ race_details/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+# Create your tests here.
+class TestRaceDetailsViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.home_url = reverse('home')
+
+    def test_homepage_GET(self):
+        response = self.client.get(self.home_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
