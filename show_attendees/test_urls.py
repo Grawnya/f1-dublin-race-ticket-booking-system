648ee4d143c48ed48694dc8ca10fc6186bdd0ac0
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-22 14:01:20 +0000

    test urls.py in show_attendees app

---
+++ show_attendees/test_urls.py
@@ -0,0 +1,9 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from show_attendees.views import ShowViableTickets
+
+# Create your tests here.
+class TestRaceDetailsUrls(SimpleTestCase):
+    def test_home_is_resolved(self):
+        url = reverse('show_attendees')
+        self.assertEquals(resolve(url).func.view_class, ShowViableTickets)
