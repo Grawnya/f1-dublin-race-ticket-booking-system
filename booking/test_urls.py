2c24e60ae7756e36c2e25b1565c9dc2d9c7e33bd
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-22 14:14:29 +0000

    test urls.py in booking app

--- booking/test_urls.py
+++ booking/test_urls.py
@@ -3,7 +3,23 @@ from django.urls import reverse, resolve
 from booking.views import *
 
 # Create your tests here.
-class TestRaceDetailsUrls(SimpleTestCase):
-    def test_home_is_resolved(self):
-        url = reverse('show_attendees')
-        self.assertEquals(resolve(url).func.view_class, ShowViableTickets)
+class TestBookingUrls(SimpleTestCase):
+    def test_profile_is_resolved(self):
+        url = reverse('profile')
+        self.assertEquals(resolve(url).func.view_class, CreateProfile)
+
+    def test_my_tickets_is_resolved(self):
+        url = reverse('my_tickets')
+        self.assertEquals(resolve(url).func.view_class, SeeMyTickets)
+
+    def test_new_ticket_is_resolved(self):
+        url = reverse('new_ticket')
+        self.assertEquals(resolve(url).func.view_class, NewTicket)
+
+    def test_edit_ticket_is_resolved(self):
+        url = reverse('edit_ticket', args=['ticket_id'])
+        self.assertEquals(resolve(url).func.view_class, EditTicket)
+
+    def test_delete_ticket_is_resolved(self):
+        url = reverse('delete_ticket', args=['ticket_id'])
+        self.assertEquals(resolve(url).func.view_class, DeleteTicket)
--- show_attendees/test_urls.py
+++ show_attendees/test_urls.py
@@ -3,7 +3,7 @@ from django.urls import reverse, resolve
 from show_attendees.views import ShowViableTickets
 
 # Create your tests here.
-class TestRaceDetailsUrls(SimpleTestCase):
+class TestShowAttendeesUrls(SimpleTestCase):
     def test_home_is_resolved(self):
         url = reverse('show_attendees')
         self.assertEquals(resolve(url).func.view_class, ShowViableTickets)
