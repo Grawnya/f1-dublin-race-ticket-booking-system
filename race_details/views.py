75e9fca3ca8c2d9ef1e80d4238d2b56d0eed092a
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-25 11:20:46 +0000

    add docstring to race_details views.py

--- race_details/views.py
+++ race_details/views.py
@@ -3,13 +3,16 @@ from django.views import generic, View
 
 # Create your views here.
 class HomePage(generic.TemplateView):
+    '''
+    A class which shows the index.html file as the home page.
+    '''
     template_name = 'index.html'
-
 
 
 def error_404(request, exception):
     """ 404 error page """
     return render(request, '404.html')
+
 
 def error_500(request):
     """ 500 error page """
