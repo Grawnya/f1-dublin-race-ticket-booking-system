3d3368867ccb10d7fc96a0c9a1b4be22967c622a
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-25 10:57:00 +0000

    add docstring to show_attendees views.py

--- show_attendees/views.py
+++ show_attendees/views.py
@@ -4,6 +4,13 @@ from booking.models import Ticket
 
 # Create your views here.
 class ShowViableTickets(generic.ListView):
+    '''
+    A class which represents all tickets where users have opted to share their ticket on the website
+
+    Methods:
+    get_queryset():
+        Gets data from the ticket Model where users decide to share their ticket and organises them in by the most recently booked.
+    '''
     model = Ticket
     template_name = 'show_attendees.html'
     paginate_by = 6
