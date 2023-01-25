ca95ff7260dd1c0c04f33c6957e6dca801b64f35
Grainne Donegan <grainnejdonegan@gmail.com>
2023-01-25 11:30:47 +0000

    update all race_details python files to align with pep8 and add docstrings

--- race_details/test_urls.py
+++ race_details/test_urls.py
@@ -4,6 +4,20 @@ from race_details.views import HomePage
 
 # Create your tests here.
 class TestRaceDetailsUrls(SimpleTestCase):
+    '''
+    A class which tests all the urls.py links.
+
+    Methods:
+    test_home_is_resolved():
+        Checks if the HomePage class is the View class used when 
+        going to the home link.
+    '''
+
+
     def test_home_is_resolved(self):
+        '''
+        Make sure the correct View class is used to return to the 
+        home page
+        '''
         url = reverse('home')
         self.assertEquals(resolve(url).func.view_class, HomePage)
--- race_details/test_views.py
+++ race_details/test_views.py
@@ -3,11 +3,28 @@ from django.urls import reverse
 
 # Create your tests here.
 class TestRaceDetailsViews(TestCase):
+    '''
+    A class which tests all the views.py functions.
+
+    Methods:
+    setUp():
+        Creates a Client instance and sets up the test to send the user to the 
+        home page if successful.
+
+    test_homepage_GET():
+        Gets the website link and if it is successful it redirects to the 
+        correct website, using the right template.
+    '''
+
+
     def setUp(self):
+        ''' Sets up test for views.py functions'''
         self.client = Client()
         self.home_url = reverse('home')
 
+
     def test_homepage_GET(self):
+        ''' Checks if the page successfully opens up the correct HTML file'''
         response = self.client.get(self.home_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
